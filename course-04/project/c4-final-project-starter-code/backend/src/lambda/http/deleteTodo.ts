fa1710911895443f76339f22b06460dcf14b92ad
Onyekachukwu Okonji <onyekaokonji@gmail.com>
2022-10-05 16:03:59 +0100

    project

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -3,16 +3,30 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
+import { createLogger } from '../../utils/logger'
 import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { getToken } from '../../auth/utils'
+const logger = createLogger('delete-todo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
-    
-    return undefined
+    logger.info('Delete Todo by Todoid')
+    try {
+      const todoId = event.pathParameters.todoId
+      // TODO: Remove a TODO item by id
+      const jwtToken = getToken(event.headers.Authorization)
+      await deleteTodo(todoId, jwtToken)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(true)
+      }
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
   }
 )
 
